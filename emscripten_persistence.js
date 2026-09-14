235daabca4967dc26e52b7b4b7c0e92ba4dc6712
f-reynaldo <freynaldo@gmail.com>
2026-09-14 10:05:54 -0300

    Fix Emscripten preRun persistence hook

--- emscripten_persistence.js
+++ emscripten_persistence.js
@@ -1,13 +1,11 @@
 // Persist fheroes2 user configuration and save games in browser IndexedDB.
 // Keep all user-writable fheroes2 files below one IDBFS mount.
-Module.preRun = Module.preRun || [];
-Module.preRun.push(function () {
+Module['preRun'] = Module['preRun'] || [];
+Module['preRun'].push(function () {
     const persistentDirectory = '/fheroes2';
 
     // fheroes2's Linux path code uses HOME/XDG_CONFIG_HOME/XDG_DATA_HOME.
-    // Force those paths into the IDBFS mount so configuration and saves are
-    // guaranteed to be persistent rather than ending up in MEMFS.
-    Object.assign(ENV, {
+    Object.assign(globalThis.ENV, {
         HOME: persistentDirectory,
         XDG_CONFIG_HOME: persistentDirectory + '/.config',
         XDG_DATA_HOME: persistentDirectory + '/.local/share'
@@ -16,12 +14,14 @@ Module.preRun.push(function () {
     FS.mkdirTree(persistentDirectory);
     FS.mount(IDBFS, { root: '/', autoPersist: true }, persistentDirectory);
 
+    console.log('fheroes2 persistence: mounting IDBFS at ' + persistentDirectory);
+
     addRunDependency('fheroes2-idbfs-load');
     FS.syncfs(true, function (error) {
         if (error) {
-            console.error('Unable to load persistent fheroes2 data:', error);
+            console.error('fheroes2 persistence: unable to load IndexedDB:', error);
         } else {
-            console.log('Loaded persistent fheroes2 data from IndexedDB at ' + persistentDirectory);
+            console.log('fheroes2 persistence: loaded IndexedDB data');
         }
         removeRunDependency('fheroes2-idbfs-load');
     });
