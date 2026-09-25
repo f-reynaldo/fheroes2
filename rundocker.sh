755915289f7fb9d043bd1c5fabca2f46d4d08910
Fabio Reynaldo <fsanchez@tecgraf.puc-rio.br>
2026-09-25 16:51:45 -0300

    Docker for building

--- rundocker.sh
+++ rundocker.sh
@@ -1,7 +1,7 @@
 docker build -t fheroes2-emscripten .
 docker run --rm -it \
-  -v /home/fsanchez/heroes2/fheroes2:/code/fheroes2 \
-  -v /home/fsanchez/heroes2/heroes2_data:/code/fheroes2_data \
+  -v /home/$USER/heroes2/fheroes2:/code/fheroes2 \
+  -v /home/$USER/heroes2/heroes2_data:/code/fheroes2_data \
   -w /code \
   fheroes2-emscripten \
   bash
